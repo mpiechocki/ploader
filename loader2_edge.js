448d0a8641144b6f862caf3465091af0f17e21fb
mpiechocki <piechocki.mikolaj@gmail.com>
2015-09-13 11:58:40 +0200

    uploading the project files

---
+++ loader2_edge.js
@@ -0,0 +1,86 @@
+
+(function(compId){var _=null,y=true,n=false,x17='29px',x66='109px',x53='RoundRect2Copy',x20='RoundRect',x46='Text3',x84='Text2Copy5',x37='289px',e36='${RoundRectCopy}',x29='12px',x55='Text3Copy',x6='dashed',x50='217px',x33='RoundRectCopy',x43='138px',x16='160px',x47='297px',i='none',x73='139px',x3='6.0.0.400',x72='114px',x26='26px',x49='36px',x23='rgba(112,110,110,1.00)',x42='76px',bg='background-color',x31='31px',x18='auto',x59='11px',x87='Text2Copy6',tp='top',x67='128px',x13='400',x78='Text2Copy3',xc='rgba(0,0,0,1)',e32='${RoundRect}',x1='6.0.0',x2='5.0.0',x41='rgba(196,21,21,1.00)',x89='6px',x86='RoundRectCopy6',zx='scaleX',x65='-12px',x38='67px',x27='9px',x34='TextCopy',x52='true',x81='Text2Copy4',x19='10px',x69='137px',x40='rgba(57,57,255,1.00)',x='text',x22='dotted',x80='RoundRectCopy4',x8='9',m='rect',x15='0px',x77='RoundRectCopy3',x75='Text2Copy2',x74='RoundRectCopy2',lf='left',x83='RoundRectCopy5',p='px',zy='scaleY',x62='Text2',x61='30px',x60='126px',x58='118px',x57='45px',x10='break-word',x9='Arial, Helvetica, sans-serif',x44='48px',l='normal',x64='13px',x68='43px',x21='rgb(0, 0, 0)',x39='RoundRect2',x24='Text',x48='75px',x30='162px',x28='108px',x5='rgba(221,221,221,1)',x4='rgba(181,181,181,1.00)',e14='${ButtonPlayAgain}',x90='125px',x12='30',x70='Text2Copy',x54='208px';var s88="<p style=\"margin: 0px;\">​<span style=\"font-family: Courier, \'Courier New\', monospace; font-size: 13px;\">ecard-recipient</span></p>",s25="<p style=\"margin: 0px;\">​Load first</p>",s56="<p style=\"margin: 0px;\">​<span style=\"font-family: \'Arial Black\', Gadget, sans-serif; color: rgb(167, 167, 167);\">PAUSE</span></p>",s51="<p style=\"margin: 0px;\">​<span style=\"font-family: \'Arial Black\', Gadget, sans-serif; color: rgb(167, 167, 167);\">PLAY AGAIN</span></p>",s45="<p style=\"margin: 0px;\">​<span style=\"font-family: \'Arial Black\', Gadget, sans-serif; color: rgb(167, 167, 167);\">SKJULE</span></p>",s85="<p style=\"margin: 0px;\">​<span style=\"font-family: Courier, \'Courier New\', monospace; font-size: 13px;\">ecard-number</span></p>",s7="<p style=\"margin: 0px;\">​LOAD</p>",s82="<p style=\"margin: 0px;\">​<span style=\"font-family: Courier, \'Courier New\', monospace; font-size: 13px;\">ecard-message4</span></p>",s79="<p style=\"margin: 0px;\">​<span style=\"font-family: Courier, \'Courier New\', monospace; font-size: 13px;\">ecard-message3</span></p>",s76="<p style=\"margin: 0px;\">​<span style=\"font-family: Courier, \'Courier New\', monospace; font-size: 13px;\">ecard-message2</span></p>",s71="<p style=\"margin: 0px;\">​<span style=\"font-family: Courier, \'Courier New\', monospace; font-size: 13px;\">ecard-message1</span></p>",s35="<p style=\"margin: 0px;\">​Load second</p>",s63="<p style=\"margin: 0px;\">​<span style=\"font-family: Courier, \'Courier New\', monospace; font-size: 13px;\">ecard-message</span></p>",s11="<p style=\"margin: 0px;\">​<span style=\"font-size: 12px;\">set:</span></p>";var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=["https://code.jquery.com/jquery-2.1.1.min.js","http://cdn.edgecommons.org/an/1.4.0/js/min/EdgeCommons.js"],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'Content',t:m,r:['101px','31px','468px','468px','auto','auto'],zr:['0px','','',''],f:[x4],s:[1,xc,i]},{id:'ButtonPlayAgain',symbolName:'ButtonPlayAgain',t:m,r:['-48px','523px','297','75','auto','auto'],cu:'pointer',tf:[[],[],[],['0.52189','0.42667']]},{id:'Button1',t:m,r:['7px','6px','178px','18px','auto','auto'],cu:'pointer',f:[x5],s:[1,"rgb(0, 0, 0)",x6],c:[{id:'Text',t:x,r:['57px','4px','64px','20px','auto','auto'],text:s7,n:[x9,[x8,p],xc,l,i,"",x10,l]}]},{id:'SetMessageButton',symbolName:'SetMessage',t:m,r:['7','608','128','43','auto','auto'],cu:'pointer'},{id:'SetMessage1Button',symbolName:'SetMessage1Button',t:m,r:['135','608','139','43','auto','auto'],cu:'pointer'},{id:'SetMessage2Button',symbolName:'SetMessage2Button',t:m,r:['274','608','139','43','auto','auto'],cu:'pointer'},{id:'SetMessage3Button',symbolName:'SetMessage3Button',t:m,r:['413','608','139','43','auto','auto'],cu:'pointer'},{id:'SetMessage4Button',symbolName:'SetMessage4Button',t:m,r:['552','608','139','43','auto','auto'],cu:'pointer'},{id:'SetNumberButton',symbolName:'SetNumberButton',t:m,r:['7','659','139','43','auto','auto'],cu:'pointer'},{id:'SetRecipientButton',symbolName:'SetRecipientButton',t:m,r:['149','659','139','43','auto','auto'],cu:'pointer'},{id:'Text4',t:x,r:['23px','578px','91px','32px','auto','auto'],text:s11,align:"left",n:[x9,[x12,p],xc,x13,i,l,x10,l],ts:["","","","",i]}],style:{'${Stage}':{isStage:true,r:['null','null','702px','761px','auto','auto'],overflow:'hidden',f:[x4]}}},tt:{d:0,a:y,data:[["eid45",zy,0,0,"linear",e14,'0.42667','0.42667'],["eid44",zx,0,0,"linear",e14,'0.52189','0.52189'],["eid55",lf,0,0,"linear",e14,'-48px','-48px'],["eid56",tp,0,0,"linear",e14,'523px','523px']]}},"Button1":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:m,r:[x15,x15,x16,x17,x18,x18],br:[x19,x19,x19,x19],id:x20,s:[1,x21,x22],f:[x23],c:[{t:x,id:x24,text:s25,r:[x26,x27,x28,x29,x18,x18],n:[x9,[11,p],xc,l,i,'',x10,l]}]}],style:{'${symbolSelector}':{r:[_,_,x30,x31]}}},tt:{d:0,a:y,data:[["eid2",bg,0,0,"linear",e32,'rgba(112,110,110,1.00)','rgba(112,110,110,1.00)']]}},"Button2":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:m,r:[x15,x15,x16,x17,x18,x18],br:[x19,x19,x19,x19],id:x33,s:[1,x21,x22],f:[x23],c:[{t:x,id:x34,text:s35,r:[x26,x27,x28,x29,x18,x18],n:[x9,[11,p],xc,l,i,'',x10,l]}]}],style:{'${symbolSelector}':{r:[_,_,x30,x31]}}},tt:{d:0,a:y,data:[["eid3",bg,0,0,"linear",e36,'rgba(112,110,110,1.00)','rgba(112,110,110,1.00)'],["eid4",lf,0,0,"linear",e36,'0px','0px']]}},"ButtonHide":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:m,r:[x15,x15,x37,x38,x18,x18],br:[x19,x19,x19,x19],id:x39,s:[4,x40,x6],f:[x41],c:[{t:x,r:[x42,x19,x43,x44,x18,x18],text:s45,id:x46,ts:['','','','',i],align:lf,n:[x9,[30,p],xc,x13,i,l,x10,l]}]}],style:{'${symbolSelector}':{r:[_,_,x47,x48]}}},tt:{d:0,a:y,data:[]}},"ButtonPlayAgain":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x15,x15,x37,x38,x18,x18],br:[x19,x19,x19,x19],id:x39,s:[4,x40,x6],t:m,f:[x41],c:[{r:[x49,x19,x50,x44,x18,x18],ts:['','','','',i],n:[x9,[30,p],xc,x13,i,l,x10,l],id:x46,text:s51,align:lf,t:x}]}],style:{'${symbolSelector}':{isStage:x52,r:[undefined,undefined,x47,x48]}}},tt:{d:0,a:y,data:[]}},"ButtonPause":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:m,id:x53,s:[4,x40,x6],r:[x15,x15,x54,x38,x18,x18],br:[x19,x19,x19,x19],f:[x41],c:[{n:[x9,[30,p],xc,x13,i,l,x10,l],t:x,align:lf,id:x55,ts:['','','','',i],text:s56,r:[x57,x19,x58,x44,x18,x18]}]}],style:{'${symbolSelector}':{r:[_,_,x47,x48]}}},tt:{d:0,a:y,data:[]}},"SetMessage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:m,id:x20,s:[1,x21,x6],r:[x15,x59,x60,x61,x18,x18],br:[x19,x19,x19,x19],f:[x5],c:[{n:[x9,[30,p],xc,x13,i,l,x10,l],t:x,align:lf,id:x62,ts:['','','','',i],text:s63,r:[x64,x65,x66,x19,x18,x18]}]}],style:{'${symbolSelector}':{r:[_,_,x67,x68]}}},tt:{d:0,a:y,data:[]}},"SetMessage1Button":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:m,id:x33,s:[1,x21,x6],r:[x15,x59,x69,x61,x18,x18],br:[x19,x19,x19,x19],f:[x5],c:[{n:[x9,[30,p],xc,x13,i,l,x10,l],t:x,align:lf,id:x70,ts:['','','','',i],text:s71,r:[x64,x65,x72,x19,x18,x18]}]}],style:{'${symbolSelector}':{r:[_,_,x73,x68]}}},tt:{d:0,a:y,data:[]}},"SetMessage2Button":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:m,id:x74,s:[1,x21,x6],r:[x15,x59,x69,x61,x18,x18],br:[x19,x19,x19,x19],f:[x5],c:[{n:[x9,[30,p],xc,x13,i,l,x10,l],t:x,align:lf,id:x75,ts:['','','','',i],text:s76,r:[x64,x65,x72,x19,x18,x18]}]}],style:{'${symbolSelector}':{r:[_,_,x73,x68]}}},tt:{d:0,a:y,data:[]}},"SetMessage3Button":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:m,id:x77,s:[1,x21,x6],r:[x15,x59,x69,x61,x18,x18],br:[x19,x19,x19,x19],f:[x5],c:[{n:[x9,[30,p],xc,x13,i,l,x10,l],t:x,align:lf,id:x78,ts:['','','','',i],text:s79,r:[x64,x65,x72,x19,x18,x18]}]}],style:{'${symbolSelector}':{r:[_,_,x73,x68]}}},tt:{d:0,a:y,data:[]}},"SetMessage4Button":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:m,id:x80,s:[1,x21,x6],r:[x15,x59,x69,x61,x18,x18],br:[x19,x19,x19,x19],f:[x5],c:[{n:[x9,[30,p],xc,x13,i,l,x10,l],t:x,align:lf,id:x81,ts:['','','','',i],text:s82,r:[x64,x65,x72,x19,x18,x18]}]}],style:{'${symbolSelector}':{r:[_,_,x73,x68]}}},tt:{d:0,a:y,data:[]}},"SetNumberButton":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:m,id:x83,s:[1,x21,x6],r:[x15,x59,x69,x61,x18,x18],br:[x19,x19,x19,x19],f:[x5],c:[{n:[x9,[30,p],xc,x13,i,l,x10,l],t:x,align:lf,id:x84,ts:['','','','',i],text:s85,r:[x64,x65,x72,x19,x18,x18]}]}],style:{'${symbolSelector}':{r:[_,_,x73,x68]}}},tt:{d:0,a:y,data:[]}},"SetRecipientButton":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:m,id:x86,s:[1,x21,x6],r:[x15,x59,x69,x61,x18,x18],br:[x19,x19,x19,x19],f:[x5],c:[{n:[x9,[30,p],xc,x13,i,l,x10,l],t:x,align:lf,id:x87,ts:['','','','',i],text:s88,r:[x89,x65,x90,x19,x18,x18]}]}],style:{'${symbolSelector}':{r:[_,_,x73,x68]}}},tt:{d:0,a:y,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-7647525");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindSymbolAction(compId,symbolName,"creationComplete",function(sym,e){function loadExtComp(name)
+{var loadedComp=EC.loadComposition(name,sym.$("Content"));loadedComp.done(function(comp)
+{loadedStage=comp.getStage();});}
+var firstName="npg0001.html";loadExtComp(firstName);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${ButtonPlayAgain}","click",function(sym,e){loadedStage.play(0);});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'Button1'
+(function(symbolName){})("Button1");
+//Edge symbol end:'Button1'
+
+//=========================================================
+
+//Edge symbol: 'Button2'
+(function(symbolName){})("Button2");
+//Edge symbol end:'Button2'
+
+//=========================================================
+
+//Edge symbol: 'ButtonHide'
+(function(symbolName){})("ButtonHide");
+//Edge symbol end:'ButtonHide'
+
+//=========================================================
+
+//Edge symbol: 'ButtonPlayAgain'
+(function(symbolName){})("ButtonPlayAgain");
+//Edge symbol end:'ButtonPlayAgain'
+
+//=========================================================
+
+//Edge symbol: 'ButtonPause'
+(function(symbolName){})("ButtonPause");
+//Edge symbol end:'ButtonPause'
+
+//=========================================================
+
+//Edge symbol: 'SetMessage'
+(function(symbolName){})("SetMessage");
+//Edge symbol end:'SetMessage'
+
+//=========================================================
+
+//Edge symbol: 'SetMessage1Button'
+(function(symbolName){})("SetMessage1Button");
+//Edge symbol end:'SetMessage1Button'
+
+//=========================================================
+
+//Edge symbol: 'SetMessage2Button'
+(function(symbolName){})("SetMessage2Button");
+//Edge symbol end:'SetMessage2Button'
+
+//=========================================================
+
+//Edge symbol: 'SetMessage3Button'
+(function(symbolName){})("SetMessage3Button");
+//Edge symbol end:'SetMessage3Button'
+
+//=========================================================
+
+//Edge symbol: 'SetMessage4Button'
+(function(symbolName){})("SetMessage4Button");
+//Edge symbol end:'SetMessage4Button'
+
+//=========================================================
+
+//Edge symbol: 'SetNumberButton'
+(function(symbolName){})("SetNumberButton");
+//Edge symbol end:'SetNumberButton'
+
+//=========================================================
+
+//Edge symbol: 'SetRecipientButton'
+(function(symbolName){})("SetRecipientButton");
+//Edge symbol end:'SetRecipientButton'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-7647525");
